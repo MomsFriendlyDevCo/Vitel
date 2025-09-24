cb49a89a64a28452c1c5c0fc93858cc592e7da90
Matt Carter <m@ttcarter.com>
2025-09-24 17:31:03 +1000

    feat($services): Can now optionally wait for services if they are declared - $services.require("$thing?")

--- services/services.vue
+++ services/services.vue
@@ -25,24 +25,39 @@ export default {
 		timeout: {type: Number, default: 0},
 	},
 	methods: {
+
 		/**
 		* Wait for all services to signal they are ready - loading any specified if necessary
 		* Services can be either a named of a service or a VueComponent
 		*
 		* A ready state is the execution of any and all init() functions and setting `{ready:true}` for each
-		* @param {...String} services Service names to wait for
+		* @param {String|Object|VueComponent...} services Services to wait for can be a String (suffix with a question mark - e.g. `$thing?`) to only await if that service has been registered) an Object definition or a VueComponent
+		*
 		* @returns {Promise} A promise when all named/specified services have become ready
 		*/
 		require(...services) {
 			return Promise.all(
 				services
 					.map(service => { // Either find existing service or load it
-						if (typeof service == 'string' && this.services[service]) {
-							return this.services[service];
+						if (typeof service == 'string') { // Service is a string...
+							let [serviceName, isOptional] = // Extract if the service is optional from '$service' vs. '$service?'
+								service.endsWith('?')
+									? [service.slice(0, -1), true]
+									: [service, false];
+
+							if (this.services[serviceName]) { // ... and it exists
+								return this.services[serviceName];
+							} else if (isOptional) { // ... it doesn't exist, but its optional anyway, return falsy and let next process filter it out
+								return false;
+							} else if (!serviceName.startsWith('$')) {
+								throw new Error(`$services.require() - waiting for non-existant service "${serviceName}" - All services should start with "$"`);
+							} else { // ... It doesn't exist and isn't optional
+								throw new Error(`$services.require() - waiting for non-existant service "${serviceName}" - is this loaded?`);
+							}
 						} else if (typeof service == 'object' && service.name && this.services[service]) { // Given VueComponent spec - but we already know it
 							return this.services[service];
 						} else if (typeof service == 'object') { // Assume VueComponent spec that needs loading
-							return  Service(service, {
+							return Service(service, {
 								app: this.app,
 							});
 						} else {
